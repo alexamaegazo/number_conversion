596032a41b2cd0894796ed604251b7b38a1da1bc
alexamaegazo <alexamaegazo5@gmail.com>
2024-09-15 09:59:45 +0800

    Initial commit with dataConversion.js

---
+++ dataConversion.js
@@ -0,0 +1,22 @@
+// CONVERT STRING TO NUMBER
+
+let price = "200";
+price = parseInt(price);
+price = Number(price);
+price = + (price);
+console.log(price);
+console.log(typeof price);
+
+//CONVERT NUMBER TO STRING
+
+let distance = 150;
+distance = distance.toString();
+console.log(distance);
+console.log(typeof distance);
+
+//CONVERT STRING TO DECIMAL NUMBER
+
+let weight = "72.5";
+weight =parseFloat(weight);
+console.log(weight);
+console.log(typeof weight);
